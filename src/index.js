4adec5b0826e51d938c70853d35d313bfbdcd328
zhenuua <zhenuua@gmail.com>
2021-03-15 23:50:26 +0300

    feat: do task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketPairs = bracketsConfig.map(pair => pair.join(''))
+  for (let i = 0; i < bracketPairs.length; ) {
+    if (str.includes(bracketPairs[i])) {
+    str = str.replace(bracketPairs[i], '')
+    i = 0
+    } else i++ 
 }
+return str === ''
+}
+
